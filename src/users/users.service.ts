5a9d3fd387fe1f4de87578788794d63fd56a7025
kcblack22@gmail.com <kcblack22@gmail.com>
2023-01-09 14:32:23 +0100

    Establishing database connection

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
-
+import { config } from 'src/db/config.db';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const knex = require('knex')(config);
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  getUser() {
+    try {
+      knex.schema.createTable(
+        'users',
+        (table) => {
+          table.increments('id').primary();
+          table.string('name');
+          table.string('email');
+          table.string('password');
+        },
+      );
+
+      return 'here on my knees to God';
+    } catch (error) {
+      console.log('Error loading users');
+    }
+  }
+  createUser(name: string, email: string) {
+    return `name: ${name}, email: ${email}`;
+  }
+}
